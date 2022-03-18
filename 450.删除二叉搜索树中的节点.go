d583be85cf496d571ea0c2876ed271f9ac55d4d6
kane <wkekai@163.com>
2022-03-18 22:03:52 +0800

    feat:days 12

---
+++ 450.删除二叉搜索树中的节点.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=golang
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == key {
+		if root.Left == nil {
+			return root.Right
+		}
+
+		if root.Right == nil {
+			return root.Left
+		}
+
+		minNode := getMin(root.Right)
+		root.Val = minNode.Val
+		root.Right = deleteNode(root.Right, minNode.Val)
+	} else if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
+	}
+
+	return root
+}
+
+func getMin(root *TreeNode) *TreeNode {
+	for root.Left != nil {
+		root = root.Left
+	}
+
+	return root
+}
+
+// @lc code=end
+
---
+++ 700.二叉搜索树中的搜索.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=golang
+ *
+ * [700] 二叉搜索树中的搜索
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	} else if root.Val > val {
+		return searchBST(root.Left, val)
+	} else if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+
+	return nil
+}
+
+// @lc code=end
+
