44d897c0be47c9a0de2cc7f924a5aef59c8a0bc1
kane <wkekai@163.com>
2022-06-01 08:42:01 +0800

    add some

---
+++ 206.反转链表.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=golang
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+
+}
+// @lc code=end
+
