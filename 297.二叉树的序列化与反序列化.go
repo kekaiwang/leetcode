b0aaed091fdfa45e5bea1b4702f8e3dafe31ff84
kane <wkekai@163.com>
2022-06-24 14:33:51 +0800

    add:postorder serialize

--- 297.二叉树的序列化与反序列化.go
+++ 297.二叉树的序列化与反序列化.go
@@ -23,6 +23,8 @@ func Constructor() (_ Codec) {
 
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *TreeNode) string {
+	// -----------------------
+	// 后序遍历方法
 	sb := &strings.Builder{}
 
 	var dfs func(*TreeNode)
@@ -33,39 +35,96 @@ func (this *Codec) serialize(root *TreeNode) string {
 			return
 		}
 
-		val := strconv.Itoa(root.Val)
-
-		sb.WriteString(val)
-		sb.WriteString(",")
-
 		dfs(root.Left)
 		dfs(root.Right)
+
+		val := strconv.Itoa(root.Val)
+		sb.WriteString(val)
+		sb.WriteString(",")
 	}
 
 	dfs(root)
 
-	return sb.String()
+	str := strings.TrimRight(sb.String(), ",")
+
+	return str
+
+	// -----------------------
+	// 前序遍历方法
+	// sb := &strings.Builder{}
+
+	// var dfs func(*TreeNode)
+
+	// dfs = func(root *TreeNode) {
+	// 	if root == nil {
+	// 		sb.WriteString("null,")
+	// 		return
+	// 	}
+
+	// 	val := strconv.Itoa(root.Val)
+
+	// 	sb.WriteString(val)
+	// 	sb.WriteString(",")
+
+	// 	dfs(root.Left)
+	// 	dfs(root.Right)
+	// }
+
+	// dfs(root)
+
+	// return sb.String()
 }
 
 // Deserializes your encoded data to tree.
 func (this *Codec) deserialize(data string) *TreeNode {
+	// -----------------------
+	// 后序遍历方法
+
 	sp := strings.Split(data, ",")
 	var build func() *TreeNode
 
 	build = func() *TreeNode {
-		if sp[0] == "null" {
-			sp = sp[1:]
+		if len(sp) == 0 {
 			return nil
 		}
 
-		val, _ := strconv.Atoi(sp[0])
+		last := sp[len(sp)-1]
+		sp = sp[:len(sp)-1]
+		if last == "null" {
+			return nil
+		}
 
-		sp = sp[1:]
+		val, _ := strconv.Atoi(last)
 
-		return &TreeNode{Val: val, Left: build(), Right: build()}
+		node := &TreeNode{Val: val}
+
+		node.Right = build()
+		node.Left = build()
+
+		return node
 	}
 
 	return build()
+
+	// -----------------------
+	// 前序遍历方法
+	// sp := strings.Split(data, ",")
+	// var build func() *TreeNode
+
+	// build = func() *TreeNode {
+	// 	if sp[0] == "null" {
+	// 		sp = sp[1:]
+	// 		return nil
+	// 	}
+
+	// 	val, _ := strconv.Atoi(sp[0])
+
+	// 	sp = sp[1:]
+
+	// 	return &TreeNode{Val: val, Left: build(), Right: build()}
+	// }
+
+	// return build()
 }
 
 /**
