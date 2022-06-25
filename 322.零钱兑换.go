642cb9d4aa836d19bafe2d10a3a9f289fec0b3ec
kane <wkekai@163.com>
2022-06-25 22:18:01 +0800

    repeat: study

--- 322.零钱兑换.go
+++ 322.零钱兑换.go
@@ -24,6 +24,25 @@ func coinChange(coins []int, amount int) int {
 	}
 
 	return dp[amount]
+
+	// dp := make([]int, amount+1)
+	// dp[0] = 0
+
+	// for i := 1; i <= amount; i++ {
+	// 	dp[i] = amount + 1
+
+	// 	for _, v := range coins {
+	// 		if i-v >= 0 {
+	// 			dp[i] = min(dp[i], 1+dp[i-v])
+	// 		}
+	// 	}
+	// }
+
+	// if dp[amount] > amount {
+	// 	return -1
+	// }
+
+	// return dp[amount]
 }
 
 func min(x, y int) int {
