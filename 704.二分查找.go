aee4e9d51777e040a0152d059eccca630859073e
kane <wkekai@163.com>
2022-06-27 17:43:34 +0800

    modify: banary

--- 704.二分查找.go
+++ 704.二分查找.go
@@ -21,6 +21,22 @@ func search(nums []int, target int) int {
 	}
 
 	return -1
+
+	// left, right := 0, len(nums)-1
+
+	// for left <= right {
+	// 	middle := left + (right-left)/2
+
+	// 	if nums[middle] == target {
+	// 		return middle
+	// 	} else if nums[middle] > target {
+	// 		right = middle - 1
+	// 	} else if nums[middle] < target {
+	// 		left = middle + 1
+	// 	}
+	// }
+
+	// return -1
 }
 
 // @lc code=end
